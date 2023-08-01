51d63abf2776c6e9f6e0e6f3c8bcadb490363e42
MdNData <nd.mirica@gmail.com>
2023-08-01 17:18:45 +0300

    added readmore functionality

--- src/assets/components/Tour/Tour.jsx
+++ src/assets/components/Tour/Tour.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 export const Tour = ({ data, setData, allData }) => {
   const { id, image, info, name, price } = data;
 
+  const [classExpand, setClassExpand] = useState("");
   const removeEntry = () => {
     setData(
       allData.filter((item) => {
@@ -12,13 +15,24 @@ export const Tour = ({ data, setData, allData }) => {
     );
   };
 
+  const changeExtend = () => {
+    if (classExpand == "") {
+      setClassExpand("expand");
+    } else {
+      setClassExpand("");
+    }
+  };
+
   return (
     <article className="single-tour" id={id}>
       <p className="tour-price">${price}</p>
       <img className="img" src={image} alt="" />
       <div className="tour-info">
         <h5>{name}</h5>
-        <p>{info}</p>
+        <p className={classExpand}>{info}</p>
+        <button onClick={changeExtend} className="delete-btn">Read More</button>
+        <br />
+        <br />
         <button onClick={removeEntry} className="btn-block delete-btn btn">
           Not Interested
         </button>
